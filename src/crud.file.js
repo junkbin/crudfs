e5f61b05890f8302f67108003a0ea3b181afed05
santosh <santosh.ece06@gmail.com>
2019-11-25 12:13:06 +0530

    crud fs.

---
+++ src/crud.file.js
@@ -0,0 +1,27 @@
+let fs = require("fs");
+
+const readFileDemo = ()=> {
+    try {
+        const fileData = fs.readFileSync("test.txt", 'utf-8');
+
+        console.log("FILE_READ :: ",  fileData);
+    } catch(err) {
+        console.log();
+    }
+}
+
+
+const writeFileDemo = () => {
+  try {
+    const data = `Data / text need to be writtern in text file`;
+    fs.writeFileSync("test.txt", data);
+    
+    console.log("FILE_WRITE :: SUCCESS ");
+  } catch (err) {
+    console.log();
+  }
+};
+
+
+
+readFileDemo();
